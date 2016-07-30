8d566b8148d92f0ba38060c4786f37d62bd24939
Isshin Toyonaga <isshintakatsusaido@gmail.com>
2016-07-30 21:52:08 +0800

    no message

---
+++ 1day/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>1st</title>
+</head>
+<body>
+	<script type="text/javascript">
+		document.write('hello,world!');
+	</script>
+</body>
+</html>
